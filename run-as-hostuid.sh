e1b8feb181e2e021f593f554e1834fb5da388727
wtanaka.com <wtanaka@users.noreply.github.com>
2020-06-27 22:22:43 -0700

    Updated copyright header

--- run-as-hostuid.sh
+++ run-as-hostuid.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # Copyright (C) 2018 Wesley Tanaka
 #
-# This file is part of docker-xenial-uid
+# This file is part of cliwrap
 #
-# docker-xenial-uid is free software: you can redistribute it
+# cliwrap is free software: you can redistribute it
 # and/or modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# docker-xenial-uid is distributed in the hope that it will be
+# cliwrap is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with docker-xenial-uid.  If not, see
+# along with cliwrap  If not, see
 # <http://www.gnu.org/licenses/>.
 
 # From http://www.etalabs.net/sh_tricks.html
